60793dfb0fa7a46248ce2489047486f91a3f6898
micro155 <qawe2006@naver.com>
2020-10-30 10:08:50 +0900

    1030

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -49,7 +49,69 @@ app.use(expressSession({
 
 
 var router = express.Router();
+
+router.route('/process/login').post(function(req, res) {
+    console.log('/process/login 라우팅 함수 호출됨.');
+    
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+    
+    if(database) {
+        authUser(database, paramId, paramPassword, function(err, docs) {
+           if(err) {
+               console.log('에러 발생.');
+               res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+               res.write('<h1>에러 발생</h1>');
+               res.end();
+               return ;
+           } 
+            
+            if(docs) {
+                console.dir(docs);
+               res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+               res.write('<h1>사용자 로그인 성공</h1>');
+                res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>');
+                res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
+               res.end();
+            } else {
+                console.log('에러 발생.');
+               res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+               res.write('<h1>사용자 데이터 조회 안됨.</h1>');
+               res.end();
+            }
+        });
+    } else {
+            console.log('에러 발생.');
+           res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+           res.write('<h1>데이터베이스 연결 안됨.</h1>');
+           res.end();
+    }
+});
+
 app.use('/',router);
+
+
+var authUser = function(db, id, password, callback) {
+    console.log('authUser 호출됨 : ' + id + ', ' + password);
+    
+    var users = db.collection('users');
+    
+    users.find({"id":id, "password":password}).toArray(function(err, docs){
+        if(err) {
+            callback(err, null);
+            return;
+        }
+        
+        if(docs.length > 0) {
+            console.log('일치하는 사용자를 찾음.');
+            callback(null, docs);
+        } else {
+            console.log('일치하는 사용자를 찾지 못함.');
+            callback(null, null);
+        }
+    });
+};
 
 
 
