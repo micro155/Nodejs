7460f3cb01945c6a0d42a8cbb1c6074db387376e
micro155 <qawe2006@naver.com>
2020-11-04 14:24:08 +0900

    1104-2

--- DatabaseExample/app8.js
+++ DatabaseExample/app8.js
@@ -86,21 +86,19 @@ router.route('/process/login').post(function(req, res) {
     var paramPassword = req.body.password || req.query.password;
     console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
     
-    if(database) {
-        authUser(database, paramId, paramPassword, function(err, docs) {
+        authUser(paramId, paramPassword, function(err, rows) {
            if(err) {
                console.log('에러 발생.');
                res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
                res.write('<h1>에러 발생</h1>');
                res.end();
                return ;
-           } 
-            
-            if(docs) {
-                console.dir(docs);
+           }
+            if(rows) {
+                console.dir(rows);
                 res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
                 res.write('<h1>사용자 로그인 성공</h1>');
-                res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>');
+                res.write('<div><p>사용자 : ' + rows[0].name + '</p></div>');
                 res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
                res.end();
             } else {
@@ -110,12 +108,6 @@ router.route('/process/login').post(function(req, res) {
                res.end();
             }
         });
-    } else {
-            console.log('에러 발생.');
-           res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
-           res.write('<h1>데이터베이스 연결 안됨.</h1>');
-           res.end();
-    }
 });
 
 
@@ -151,26 +143,41 @@ var addUser = function(id, name, age, password, callback) {
 };
 
 
-var authUser = function(db, id, password, callback) {
+var authUser = function(id, password, callback) {
     console.log('authUser 호출됨 : ' + id + ', ' + password);
     
-    var users = db.collection('users');
-    
-    users.find({"id" : id, "password" : password}).toArray(function(err, docs) {
+    pool.getConnection(function(err, conn) {
         if(err) {
+            if(conn) {
+                conn.release();
+            }
             callback(err, null);
             return;
         }
-        if(docs.length > 0) {
-            console.log('일치하는 사용자를 찾음.');
-            callback(null, docs);
-        } else {
-            console.log('일치하는 사용자를 찾지 못함');
-            callback(null, null);
+        console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+        
+        var tablename = 'users';
+        var columns = ['id', 'name', 'age'];
+        var exec = conn.query("select ?? from ?? where id = ? and password = ?", [columns, tablename, id, password], function(err, rows) {
+            conn.release();
+            console.log('실행된 SQL : ' + exec.sql);
+            
+            if(err)
+                {
+                    callback(err, null);
+                    return;
+                }
+            if(rows.length > 0) {
+                console.log('사용자 찾지 못함.');
+                callback(null, rows);
+            } else {
+                console.log('사용자 찾지 못함.');
+                callback(null, null);
+            }
+        });
+    })
+    
         }
-    });
-};
-
 
 
 var errorHandler = expressErrorHandler({
