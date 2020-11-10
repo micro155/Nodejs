1e8c77220a757c76d820c1bf6ea288462ce9d2d7
micro155 <qawe2006@naver.com>
2020-11-10 23:44:26 +0900

    1110-2

---
+++ ChatExample/app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+
+//에러 핸들러 사용
+var expressErrorHandler = require('express-error-handler');
+
+var crypto = require('crypto');
+
+//===== socket.io 사용 =====//
+
+var socketio = require('socket.io');
+var cors = require('cors');
+
+
+//===== Passport 사용 =====//
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var user = require('./routes/user');
+
+var config = require('./config/config');
+
+var database_loader = require('./database/database_loader');
+var route_loader = require('./routes/route_loader');
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+
+
+console.log('config.server_port -> ' + config.server_port);
+app.set('port', config.server_port || 3000);
+
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'my key',
+    resave: true,
+    saveUninitialized:true
+}));
+
+//===== cors 초기화 =====//
+app.use(cors());
+
+
+//===== passport 초기화 =====//
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+var configPassport = require('./config/passport');
+configPassport(app, passport);
+
+var router = express.Router();
+route_loader.init(app, router);
+
+var userPassport = require('./routes/user_passport');
+userPassport(router, passport);
+
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'));
+    
+    database_loader.init(app, config);
+});
+
+
+//===== socket.io 서버 시작 =====//
+var io = socketio.listen(server);
+console.log('socket.io 요청을 받아들일 준비가 되었습니다.');
